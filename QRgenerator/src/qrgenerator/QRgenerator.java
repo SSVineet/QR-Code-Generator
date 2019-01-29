aefef773bd82008a986b130e3fa28fd41be7ed3e
Maurya Vineet <44580462+vksmpic@users.noreply.github.com>
2019-01-29 19:29:22 +0530

    Add files via upload

---
+++ QRgenerator/src/qrgenerator/QRgenerator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qrgenerator;
+
+/**
+ *
+ * @author MAURYA
+ */
+public class QRgenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        QRCODE obj = new QRCODE();
+        obj.setResizable(false);
+        obj.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
